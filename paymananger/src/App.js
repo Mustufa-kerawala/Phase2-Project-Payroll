170039ad1ffc6df9df7abd8f0d45346a0cc4331b
Mustufa-kerawala <mustufakerawala@outlook.com>
2023-11-21 14:59:26 +0300

    Created a PayrollList component which gets data and renders it in a table

--- paymananger/src/App.js
+++ paymananger/src/App.js
@@ -1,10 +1,31 @@
 import './App.css';
+import PayrollList from './Components/PayrollList';
+import React, {useEffect, useState} from 'react';
 
 function App() {
+
+  // Holding all staff data in state for now
+  const [staff, setStaff] = useState([]);
+
+  // Getting all data for staff members from API
+  const getStaff = () => {
+    fetch('http://localhost:3001/staff')
+    .then(response => response.json())
+    .then(data => setStaff(data));
+  }
+
+  useEffect(() => {
+    getStaff();
+  }, []);
+
+
+  
+
+
   return (
     <div className="App">
-      <h1>Pay Manager</h1>
-      
+      <h1>Payroll Manager</h1>
+      <PayrollList staffData={staff}/>
     </div>
   );
 }
